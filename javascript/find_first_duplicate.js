03023335bea756ac65be8c82ed03744d0dc9e173
Kane Mathis <admin@kanemathis.com>
2022-08-28 15:35:56 -0400

    commit tree 2e57389da547242a401f092f61ac4a189a6dd52d

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,13 +1,27 @@
 function findFirstDuplicate(arr) {
-  // type your code here
-}
+  for (let index in arr){
+    let x=parseInt(index)+1;
+    
+      if (arr[index]===arr[x]){
+       
+        return (arr[index]);
+      } 
+      else if (x===arr.length){
+        
+        return(-1);
+      }
+    }
+  }
+
+
 
 if (require.main === module) {
+  console.log("Expecting: 7");
+  console.log("=>", findFirstDuplicate([2, 1, 6, 3, 2, 7, 7]));
+
   // add your own tests in here
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
-
-  console.log("");
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
@@ -15,5 +29,4 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// for each index of an array I will see if it matches any other index and if it does I will have it return that index and then stop checking since the instructions are to return the first match. And I am assuming find 'first' means when reading left to right. I will iterate through the array and check the index next to the current index by making the index an integer an adding 1 to it then using an if statement to see if the current index has the same value as that index plus 1, or the next index. 
